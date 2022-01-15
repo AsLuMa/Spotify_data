c50ce36190a81540141fe4d1201e9c14247574da
asluma <ase.mattson@gmail.com>
2022-01-15 17:23:09 +0100

    Cleaned up script, added functions to get data about individual artists

--- Spotify_data_with_pandas.py
+++ Spotify_data_with_pandas.py
@@ -1,30 +1,72 @@
 import json
 import pandas as pd
-import math
+
+# TODO When do I listen? When do I listen to Gloryhammer? How do aggregate this? (Month, time of day?)
+# TODO How many times have I listened to all? What are the top 10 artists I listen to the most?
 
 with open('StreamingHistory0.json', 'r', encoding='utf8') as file:
     data = json.load(file)
 
 df = pd.DataFrame(data)
-# print(df.head(10).to_string())
 
-# Group by artist name, get all Gloryhammer-info, sort by name of song, then set index to artist name
-group_an = df.groupby(by='artistName')
-df_gh = group_an.get_group('Gloryhammer').sort_values(by='trackName')
-# print(df_gh.head(10).to_string())
-df_gh = df_gh.set_index('artistName')
-# print(df_gh.head(10).to_string())
+# --- # All songs I've listened to for over two minutes, with milliseconds-played column dropped
+df_over_two_mins = df.loc[df['msPlayed'] > 120000]
+df_over_two_mins = df_over_two_mins.set_index('artistName').drop('msPlayed', axis=1).sort_values(by='trackName')
 
-# Find all songs that have been played more than 2 minutes
-df_over_12000 = df_gh.loc[df_gh['msPlayed'] > 120000]
-print(df_over_12000)
+top_ten_artists = df_over_two_mins.index.value_counts().head(10)
+print(top_ten_artists)
 
-
-def ms_to_second(ms):
-    return math.floor(ms/1000)
+# list_all_bands = df.loc[:, 'artistName'].unique()
+# print(list_all_bands)
 
 
 exit()
+
+# --- # Various functions
+def songs_by_one_band(group, band_name):
+    df_song_by_band = group.get_group(band_name)
+    return df_song_by_band
+
+def print_songs_by_one_band(group, band_name):
+    df_song_by_band = group.get_group(band_name)
+    for i in df_song_by_band['trackName'].unique():
+        print(i)
+
+def print_played_more_than_ten_times(df_artist):
+    nr_times_track_played = df_artist['trackName'].value_counts()
+    track_over_ten = nr_times_track_played.loc[lambda x: x > 10]
+    print(track_over_ten)
+
+def print_top_ten_by_artist(df_artist):
+    top_ten_tracks = df_artist['trackName'].value_counts().head(10)
+    print(top_ten_tracks)
+
+# --- # Fetch some data
+# Need: Groupby-object, band-name, datafram (from Groupby-object)
+
+# Groupby
+group_an = df_over_two_mins.groupby(by='artistName')
+
+# Dataframes from groupby
+df_gh = songs_by_one_band(group_an, 'Gloryhammer')
+df_sb = songs_by_one_band(group_an, 'Sabaton')
+df_ghost = songs_by_one_band(group_an, 'Ghost')
+
+# Function calls
+print_played_more_than_ten_times(df_sb)
+print_played_more_than_ten_times(df_gh)
+print_played_more_than_ten_times(df_ghost)
+
+print_songs_by_one_band(group_an, 'Sabaton')
+print_songs_by_one_band(group_an, 'Gloryhammer')
+
+# print_top_ten_by_artist(df_gh)
+# print_top_ten_by_artist(df_sb)
+
+
+
+exit()
+
 
 # Finding most frequent songs played without Groupby
 df = df.set_index('artistName')
